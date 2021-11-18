819c8ad63c52672daf5b3cccceee548203e85add
Paris Pray <paris.pray@gmail.com>
2021-11-18 12:49:07 -0700

    Color change script pass3

--- colorchangescript.py
+++ colorchangescript.py
@@ -1,21 +1,31 @@
 import maya.cmds as cmds
 
+##def changeColor():
+    ##objSel = cmds.ls(selection=True)
+    ##cmds.setAttr ('objSel' + ".overrideEnabled", 0)
+    ##cmds.setAttr ('objSel' + ".overrideColor", 13)
 def changeColor():
-    cmds.setAttr ('objSel' + ".overrideEnabled", 1)
-    cmds.setAttr ('objSel' + ".overrideColor", 13)
+    colorSel = cmds.ls(sl=True)
+    cmds.setAttr ('colorSel' + ".overrideEnabled", 0)
+    cmds.setAttr ('colorSel' + ".overrideColor", 13)
+    
     
 def createControl():
-    objSel=cmds.ls(sl=True)
     cmds.circle (n='ctrl')
-    cmds.group ('ctrl',name='ctrlGroup')
-    cmds.matchTransform('ctrlGroup',objSel)
+    objSel = cmds.ls(selection=True)
+    cmds.group ('ctrl',name='GroupJnt')
+    cmds.matchTransform('GroupJnt',objSel)
 
 
 def renameFunc(string):
-    listObj = cmds.ls(selection=True)
+    objSel = cmds.ls(selection=True)
     string.partition("##")
-    for count, object in enumerate(listObj):
+    for count, object in enumerate(objSel):
         cmds.rename(object, string.partition('#')[0] + str(count + 1).zfill(string.count('#')) + string.rpartition('#')[2])
+
+createControl()
+cmds.select ("ctrl*")
 renameFunc("Arm_##_Jnt")
-changeColor()
+cmds.select("GoupJnt")
+renameFunc("Arm_##_Grp")
 
