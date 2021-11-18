9017a09dbe097505f6db44733dda2d9acfd21280
Paris Pray <paris.pray@gmail.com>
2021-11-18 09:24:26 -0700

    Color change script pass2

--- colorchangescript.py
+++ colorchangescript.py
@@ -1,11 +1,21 @@
 import maya.cmds as cmds
 
-def changeColor()
-crvSel = cmds.ls(sl=true)
-cmds.setAttr(crvSel + ".overrideEnabled", 1)
-cmds.setAttr(crvSel + "overrideRGBColors", 0)
-cmds.optionMenu(omenu, q=True, v=True)
-val=cDict.get(key, 0)
-cmds.setAttr(crvSel + ".overrideColor", val)
+def changeColor():
+    cmds.setAttr ('objSel' + ".overrideEnabled", 1)
+    cmds.setAttr ('objSel' + ".overrideColor", 13)
+    
+def createControl():
+    objSel=cmds.ls(sl=True)
+    cmds.circle (n='ctrl')
+    cmds.group ('ctrl',name='ctrlGroup')
+    cmds.matchTransform('ctrlGroup',objSel)
 
+
+def renameFunc(string):
+    listObj = cmds.ls(selection=True)
+    string.partition("##")
+    for count, object in enumerate(listObj):
+        cmds.rename(object, string.partition('#')[0] + str(count + 1).zfill(string.count('#')) + string.rpartition('#')[2])
+renameFunc("Arm_##_Jnt")
 changeColor()
+
