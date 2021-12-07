a09c9f87bf7339a0a38fc8878a479568ad6dbd40
Paris Pray <paris.pray@gmail.com>
2021-12-07 09:10:25 -0700

    group selection fixed, start UI work

--- colorchangescript.py
+++ colorchangescript.py
@@ -1,24 +1,29 @@
 import maya.cmds as cmds
 
+
 def changeColor():
     colorSel = cmds.ls(sl=True)
-    cmds.setAttr (colorSel[0] + ".overrideEnabled", 1)
-    cmds.setAttr (colorSel[0] + ".overrideColor", 13)
-  
-    
+    cmds.setAttr(colorSel[0] + ".overrideEnabled", 1)
+    cmds.setAttr(colorSel[0] + ".overrideColor", 13)
+
+
 def createControl():
-    objSel = cmds.ls(selection=True)
-    cmds.circle (n='ctrl')
-    cmds.group ('ctrl',name='GroupJnt')
-    cmds.matchTransform('GroupJnt',objSel)
-    
+    objsel = cmds.ls(sl=True)
+    for obj in objsel:
+        circle = cmds.circle(n='ctrl')
+        group = cmds.group(circle, name='GroupJnt')
+        cmds.matchTransform(group, obj)
+    print(objSel)
+
 
 
 def renameFunc(string):
     objSel = cmds.ls(selection=True)
     string.partition("##")
     for count, object in enumerate(objSel):
-        cmds.rename(object, string.partition('#')[0] + str(count + 1).zfill(string.count('#')) + string.rpartition('#')[2])
+        cmds.rename(object,
+                    string.partition('#')[0] + str(count + 1).zfill(string.count('#')) + string.rpartition('#')[2])
+
 
 createControl()
 cmds.select("GroupJnt")
@@ -28,6 +33,5 @@ renameFunc("Arm_##_Ctrl")
 cmds.select("Arm_##_Ctrl")
 changeColor()
 
-    
-    
+
 
